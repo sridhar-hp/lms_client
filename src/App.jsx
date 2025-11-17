79485a451f536a3ee0cd88cb32357aa802019b0c
sridhar-hp <sridhark3773@gmail.com>
2025-11-17 21:01:16 +0530

    ui design for login

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LoginPage from "./pages/LoginPage";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      
+    </Routes>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App;
