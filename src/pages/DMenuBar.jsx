e01955d4b8af3e9481cc5b5313e6c752253d1294
sridhar-hp <sridhark3773@gmail.com>
2025-11-19 21:38:05 +0530

    dmenu

--- src/pages/DMenuBar.jsx
+++ src/pages/DMenuBar.jsx
@@ -1,22 +1,34 @@
-import React from "react";
-import { NavLink,Outlet } from "react-router-dom";
+// import React from "react";
+// import { NavLink,Outlet } from "react-router-dom";
 
-function DMenuBar() {
-    return (
-        <>
-            <div>
-                <div >
-                    <NavLink to="DashBoard">My Dashboard</NavLink>
-                    <NavLink to="LeaveRequests"> LeaveRequests </NavLink>
-                    <NavLink to="LeaveApply">LeaveApply</NavLink>
-                    <NavLink to="TeamView"> TeamView</NavLink>
-                </div>
-                <div >
-                    <Outlet />
-                </div>
-            </div>
-        </>
+// function DMenuBar() {
+//     return (
+//         <>
+//             <div>
+//                 <div >
+//                     <NavLink to="DashBoard">My Dashboard</NavLink>
+//                     <NavLink to="LeaveRequests"> LeaveRequests </NavLink>
+//                     <NavLink to="LeaveApply">LeaveApply</NavLink>
+//                     <NavLink to="TeamView"> TeamView</NavLink>
+//                 </div>
+//                 <div >
+//                     <Outlet />
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+// export default DMenuBar;
+
+
+import React from "react";
+
+
+function DMenuBar(){
+
+    const admin
+    return(
+        <div>DMenuBar Page</div>
     );
 }
 export default DMenuBar;
-
