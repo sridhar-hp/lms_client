6bf9659c295ed04a29a9a69611fc9741893a3eeb
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:48:56 +0530

    done

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true // this ensures cookies are sent
+    baseURL: "http://localhost:5000/api",
+    withCredentials: true // this ensures cookies are sent
 });
 
 export default axiosClient;
