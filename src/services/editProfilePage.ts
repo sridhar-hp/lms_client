4331966fa942d94f0d3267745d822fe54376e286
sridhar-hp <sridhark3773@gmail.com>
2026-05-13 21:07:06 +0530

    bug fixed

--- src/services/editProfilePage.ts
+++ src/services/editProfilePage.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const API = "http://localhost:5000/api";
 
-export const pdetails = (userId:any, token:any) => {
+export const pdetails = (userId: any, token: any) => {
     return axios.get(`${API}/profile/${userId}`, {
-        headers:{
-            Authorization:`Bearer ${token}`
+        headers: {
+            Authorization: `Bearer ${token}`
         }
-        });
+    });
 };
